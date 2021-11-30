fd0e9381d606b408e956ef8c2c023cb2219af68d
HienNguyen120201 <90005023+HienNguyen120201@users.noreply.github.com>
2021-11-30 10:15:21 +0700

    Thái xong

--- SHIPPER/SHIPPER/Models/KhachHangViewModel.cs
+++ SHIPPER/SHIPPER/Models/KhachHangViewModel.cs
@@ -11,19 +11,18 @@ namespace SHIPPER.Models
         [Required(ErrorMessage = "Vui lòng điền Chứng minh hoặc căn cước")]
         public int Cmnd { get; set; }
         [Required(ErrorMessage = "Vui lòng điền Họ")]
-        [RegularExpression(@"^[a-z]$", ErrorMessage = "Họ chỉ được có chữ thường và chữ in hoa")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Họ chỉ được có chữ thường và chữ in hoa")]
         public string Ho { get; set; }
-        [RegularExpression(@"^[a-z][A-Z]$", ErrorMessage = "Tên lót chỉ được có chữ thường và chữ in hoa")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Tên lót chỉ được có chữ thường và chữ in hoa")]
         public string TenLot { get; set; }
         [Required(ErrorMessage = "Vui lòng điền Tên")]
-        [RegularExpression(@"^[a-z][A-Z]$", ErrorMessage = "Tên chỉ được có chữ thường và chữ in hoa")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Tên chỉ được có chữ thường và chữ in hoa")]
         public string Ten { get; set; }
         [Required(ErrorMessage = "Vui lòng điền Ngày Sinh")]
         public DateTime NgaySinh { get; set; }
         [Required(ErrorMessage = "Vui lòng điền Giới Tính")]
         public string GioiTinh { get; set; }
         [Required(ErrorMessage = "Vui lòng điền Tài Khoản")]
-        [RegularExpression(@"^[\s]$", ErrorMessage = "Tên tài khoản không được có khoảng trắng")]
         public string TaiKhoan { get; set; }
         [Required(ErrorMessage = "Vui lòng điền Mật Khẩu")]
         [StringLength(20, MinimumLength = 8, ErrorMessage = "Mật khẩu phải từ 8-20 kí tự")]
